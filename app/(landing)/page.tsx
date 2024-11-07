600e94613eb6ac109023c608414bdf1582d6dce6
lou1s vuong <lou1svuong.dev@gmail.com>
2024-11-07 11:34:17 +0700

    Add Home component with HeroSection and FeatureSection

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+import FeatureSection from "@/containers/home-page/feature-section";
+import HeroSection from "@/containers/home-page/hero-section";
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      <FeatureSection />
+    </>
+  );
+}
