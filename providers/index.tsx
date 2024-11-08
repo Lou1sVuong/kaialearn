921735d520ae3ed231d75040a53cc790c1e0d588
lou1s vuong <lou1svuong.dev@gmail.com>
2024-11-09 00:39:34 +0700

    Wrap children in WalletProviders within the Providers component for wallet context integration

--- providers/index.tsx
+++ providers/index.tsx
@@ -1,9 +1,10 @@
 import { ThemeProvider } from "@/providers/theme-provider";
+import { WalletProviders } from "@/providers/wallet-provider";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      {children}
+      <WalletProviders>{children}</WalletProviders>
     </ThemeProvider>
   );
 }
