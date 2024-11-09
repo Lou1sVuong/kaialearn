a11e8bd814795e97064821887a9ff2fb1aba3a5f
lou1s vuong <lou1svuong.dev@gmail.com>
2024-11-09 16:10:25 +0700

    Update HeroSection to apply custom selection styles to the Cover component

--- containers/home-page/hero-section/index.tsx
+++ containers/home-page/hero-section/index.tsx
@@ -6,8 +6,8 @@ export default function HeroSection() {
     <section className="mt-10 border border-border p-16 lg:mt-20">
       <div className="flex h-full min-h-[30rem] w-full flex-col justify-center gap-5 lg:items-center lg:text-center">
         <h1 className="text-5xl font-bold uppercase lg:px-28 lg:text-5xl">
-          Hack, learn, and secure the future of <Cover>decentralized</Cover>{" "}
-          tech.
+          Hack, learn, and secure the future of{" "}
+          <Cover className="select-none">decentralized</Cover> tech.
         </h1>
         <p className="w-[90%] text-sm font-medium lg:w-2/3 lg:text-sm">
           Master smart contract security through hands-on puzzles. Exploit
