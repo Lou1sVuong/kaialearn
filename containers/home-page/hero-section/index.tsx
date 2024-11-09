f0de959080e5dc9c89c4f621871807b1bdbf63b4
lou1s vuong <lou1svuong.dev@gmail.com>
2024-11-10 03:49:09 +0700

    Refactor Select and Toast components for consistency; update HeroSection layout and link

--- containers/home-page/hero-section/index.tsx
+++ containers/home-page/hero-section/index.tsx
@@ -3,7 +3,7 @@ import { LinkButton } from "@/components/ui/link-button";
 
 export default function HeroSection() {
   return (
-    <section className="mt-10 border border-border p-16 lg:mt-20">
+    <section className="mt-12 border border-border p-16 lg:mt-14">
       <div className="flex h-full min-h-[30rem] w-full flex-col justify-center gap-5 lg:items-center lg:text-center">
         <h1 className="text-5xl font-bold uppercase lg:px-28 lg:text-5xl">
           Hack, learn, and secure the future of{" "}
@@ -15,7 +15,7 @@ export default function HeroSection() {
           hacks.
         </p>
         <div className="flex h-12 w-2/3 gap-5 lg:w-1/3">
-          <LinkButton className="h-full flex-1" href="/puzzles">
+          <LinkButton className="h-full flex-1" href="/learn">
             Try It Now
           </LinkButton>
           <LinkButton variant="outline" className="h-full flex-1" href="/docs">
