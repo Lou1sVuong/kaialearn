4b93adaec558604b737b6ffd9a0ceb274e263e3b
lou1s vuong <lou1svuong.dev@gmail.com>
2024-11-09 17:04:00 +0700

    Add PuzzlesHeader component to PuzzlesLayout for improved structure

--- app/(puzzles)/puzzles/layout.tsx
+++ app/(puzzles)/puzzles/layout.tsx
@@ -1,7 +1,14 @@
+import PuzzlesHeader from "@/components/layouts/puzzles/header";
+
 export default function PuzzlesLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <main className="flex flex-col gap-24 xl:px-20">{children}</main>;
+  return (
+    <main className="flex flex-col xl:px-20">
+      <PuzzlesHeader />
+      {children}
+    </main>
+  );
 }
