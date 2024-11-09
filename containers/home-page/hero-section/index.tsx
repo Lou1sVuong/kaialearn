16fb31067feeaf71883ff92ffec35d420a6d1820
lou1s vuong <lou1svuong.dev@gmail.com>
2024-11-10 05:03:57 +0700

    Update HeroSection component for improved spacing and typography

--- containers/home-page/hero-section/index.tsx
+++ containers/home-page/hero-section/index.tsx
@@ -3,13 +3,13 @@ import { LinkButton } from "@/components/ui/link-button";
 
 export default function HeroSection() {
   return (
-    <section className="mt-12 border border-border p-16 lg:mt-14">
+    <section className="mt-12 border border-border p-4 lg:mt-14 lg:p-16">
       <div className="flex h-full min-h-[30rem] w-full flex-col justify-center gap-5 lg:items-center lg:text-center">
-        <h1 className="text-5xl font-bold uppercase lg:px-28 lg:text-5xl">
+        <h1 className="text-4xl font-bold uppercase lg:px-28 lg:text-5xl">
           Hack, learn, and secure the future of{" "}
           <Cover className="select-none">decentralized</Cover> tech.
         </h1>
-        <p className="w-[90%] text-sm font-medium lg:w-2/3 lg:text-sm">
+        <p className="w-[95%] text-xs font-medium lg:w-2/3 lg:text-sm">
           Master smart contract security through hands-on puzzles. Exploit
           vulnerabilities, learn best practices, and earn rewards for successful
           hacks.
