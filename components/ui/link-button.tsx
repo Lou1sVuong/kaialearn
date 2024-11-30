60eaaadab11350475e82eb0bd1c52b359081fa4a
lou1s vuong <lou1svuong.dev@gmail.com>
2024-11-30 16:58:19 +0700

    Enhance LinkButton component: add blankTarget prop for opening links in a new tab

--- components/ui/link-button.tsx
+++ components/ui/link-button.tsx
@@ -3,17 +3,20 @@ import { cn } from "@/lib/utils";
 import { VariantProps } from "class-variance-authority";
 import React from "react";
 import Link from "next/link";
+import { Target } from "lucide-react";
 
 export interface LinkButtonProps
   extends React.LinkHTMLAttributes<HTMLAnchorElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
+  blankTarget?: boolean;
 }
 
 export const LinkButton = React.forwardRef<HTMLAnchorElement, LinkButtonProps>(
-  ({ className, variant, size, href, ...props }, ref) => {
+  ({ className, variant, size, href, blankTarget, ...props }, ref) => {
     return (
       <Link
+        target={blankTarget ? "_blank" : undefined}
         href={href || ""}
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
